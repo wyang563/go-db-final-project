449d807fafc85c48541b52cb20b8441cf88fc99a
wyang563 <willyanglex@gmail.com>
2023-12-13 20:09:33 -0500

    final version of lab 4

--- godb/benchmark_test.go
+++ godb/benchmark_test.go
@@ -1,12 +1,12 @@
 package godb
 
 import (
-	"time"
-	"strconv"
-	"math/rand"
 	"fmt"
-	"testing"
+	"math/rand"
 	"os"
+	"strconv"
+	"testing"
+	"time"
 )
 
 // make random range queries, call it on btree, call it on heapfile, have to add tuples to heapfile (can do sequentially)
@@ -16,13 +16,13 @@ func TestBTreeTime(t *testing.T) {
 		{Fname: "name", Ftype: StringType},
 		{Fname: "age", Ftype: IntType},
 	}}
-	var tupleList []*Tuple;
-	divideField := FieldExpr{selectField: desc.Fields[1]};
+	var tupleList []*Tuple
+	divideField := FieldExpr{selectField: desc.Fields[1]}
 	b_factor := 10
 	tid := NewTID()
 	bp := NewBufferPool(3)
 	os.Remove(TestingFile)
-	heapfile, _ := NewHeapFile(TestingFile, &desc, bp);
+	heapfile, _ := NewHeapFile(TestingFile, &desc, bp)
 	btree, _ := NewBtreeFile("", &desc, b_factor, divideField)
 
 	// if err != nil {
@@ -30,58 +30,51 @@ func TestBTreeTime(t *testing.T) {
 	// }
 
 	for i := 0; i < 10000; i++ {
-		name := "sam" + strconv.Itoa(i);
-		tup := Tuple{Desc: desc, 
-					Fields: []DBValue{
-						StringField{name},
-						IntField{int64(i)},
-					}}
-		tupleList = append(tupleList, &tup);
+		name := "sam" + strconv.Itoa(i)
+		tup := Tuple{Desc: desc,
+			Fields: []DBValue{
+				StringField{name},
+				IntField{int64(i)},
+			}}
+		tupleList = append(tupleList, &tup)
 		heapfile.insertTuple(&tup, tid)
 	}
 
 	btree.init(tupleList)
-	btreeTime := 0;
-	heapTime := 0;
-	iters := 100;
-	width := 100;
+	btreeTime := 0
+	heapTime := 0
+	iters := 1000
 	for i := 0; i < iters; i++ {
 		s := rand.NewSource(time.Now().UnixNano())
 		r := rand.New(s)
-		lval := r.Intn(1000)
-		var rval int;
-		if (lval + width < 1000) {
-			rval = lval + width;
-		} else {
-			rval = 999;
-		}
-
+		lval := r.Intn(9000)
+		width := r.Intn(100)
+		rval := lval + width
 		left, right := tupleList[lval], tupleList[rval]
-		
+
 		btreeIter, _ := btree.SelectRange(left, right, tid)
-		heapfileIter, _ := heapfile.Iterator(tid);
+		heapfileIter, _ := heapfile.Iterator(tid)
 		// time b+tree range query
-		start := time.Now();
-		tup, _ := btreeIter();
+		start := time.Now()
+		tup, _ := btreeIter()
 		for tup != nil {
-			tup, _ = btreeIter();
+			tup, _ = btreeIter()
 		}
-		elapsed := time.Since(start);
-		btreeTime += int(elapsed);
+		elapsed := time.Since(start)
+		btreeTime += int(elapsed)
 		// time heapfile range query
-		start = time.Now();
-		tup, _ = heapfileIter();
+		start = time.Now()
+		tup, _ = heapfileIter()
 		count := 0
 		for tup != nil {
-			tup, _ = heapfileIter();
-			count++;
+			tup, _ = heapfileIter()
+			count++
 		}
-		elapsed = time.Since(start);
-		heapTime += int(elapsed);
+		elapsed = time.Since(start)
+		heapTime += int(elapsed)
 	}
-	fmt.Println(heapfile.bufPool);
-	fmt.Println("Average heap Time is - ", heapTime / iters);
-	fmt.Println("Average b+tree Time is - ", btreeTime / iters);
+	fmt.Println(heapfile.bufPool)
+	fmt.Println("Average heap Time is - ", heapTime/iters)
+	fmt.Println("Average b+tree Time is - ", btreeTime/iters)
 	// return nil;
 }
-
